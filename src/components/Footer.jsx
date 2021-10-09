ec57bea7e6bba531d0080db0f9e24e0591b9ffb0
Lindomar <lsantosmanga21@gmail.com>
2021-10-09 14:55:47 -0300

    Inserindo footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>
+        <a href="https://github.com/Lsantos-art" className="link">
+          Desenvolvido por Lsantos-art®
+        </a>
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -2,6 +2,7 @@ import Button from "../components/Button";
 import Display from "../components/Display";
 import React from "react";
 import "./Calculator.css";
+import Footer from "../components/Footer";
 
 const initialState = {
   displayValue: "0",
@@ -27,6 +28,7 @@ class Calculator extends React.Component {
 
   setOperation(operation) {
     if (this.state.current === 0) {
+      operation = operation !== "=" ? operation : "+";
       this.setState({ operation, current: 1, clearDisplay: true });
     } else {
       const equals = operation === "=";
@@ -70,25 +72,28 @@ class Calculator extends React.Component {
 
   render() {
     return (
-      <div className="calculator">
-        <Display value={this.state.displayValue} />
-        <Button label="AC" click={this.clearMemory} triple />
-        <Button label="/" click={this.setOperation} operation />
-        <Button label="7" click={this.addDigit} />
-        <Button label="8" click={this.addDigit} />
-        <Button label="9" click={this.addDigit} />
-        <Button label="*" click={this.setOperation} operation />
-        <Button label="4" click={this.addDigit} />
-        <Button label="5" click={this.addDigit} />
-        <Button label="6" click={this.addDigit} />
-        <Button label="-" click={this.setOperation} operation />
-        <Button label="1" click={this.addDigit} />
-        <Button label="2" click={this.addDigit} />
-        <Button label="3" click={this.addDigit} />
-        <Button label="+" click={this.setOperation} operation />
-        <Button label="0" click={this.addDigit} double />
-        <Button label="." click={this.addDigit} />
-        <Button label="=" click={this.setOperation} operation />
+      <div className="content">
+        <div className="calculator">
+          <Display value={this.state.displayValue} />
+          <Button label="AC" click={this.clearMemory} triple />
+          <Button label="/" click={this.setOperation} operation />
+          <Button label="7" click={this.addDigit} />
+          <Button label="8" click={this.addDigit} />
+          <Button label="9" click={this.addDigit} />
+          <Button label="*" click={this.setOperation} operation />
+          <Button label="4" click={this.addDigit} />
+          <Button label="5" click={this.addDigit} />
+          <Button label="6" click={this.addDigit} />
+          <Button label="-" click={this.setOperation} operation />
+          <Button label="1" click={this.addDigit} />
+          <Button label="2" click={this.addDigit} />
+          <Button label="3" click={this.addDigit} />
+          <Button label="+" click={this.setOperation} operation />
+          <Button label="0" click={this.addDigit} double />
+          <Button label="." click={this.addDigit} />
+          <Button label="=" click={this.setOperation} operation />
+        </div>
+        <Footer />
       </div>
     );
   }
